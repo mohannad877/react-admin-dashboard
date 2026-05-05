bc07136ecd36e6ae07836c11c6fb6a25cbab85b4
mohannad877 <mhndnbyl498@gmail.com>
2026-05-06 00:38:54 +0300

    fix: move import statement before initSentry call to fix syntax error

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { ThemeProvider } from './application/contexts/ThemeContext';
 import { TenantProvider } from './application/contexts/TenantProvider';
 import { initSentry } from './shared/config/sentry';
 import './shared/config/i18n';
-
-initSentry();
 import App from './App';
 import './index.css';
+
+initSentry();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
